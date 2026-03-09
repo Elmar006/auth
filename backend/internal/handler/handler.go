995fb5fefe8df5961dcf5fada28461cefe8a528d
Elmar Birembekov <birembekove@gmail.com>
2026-03-09 23:34:34 +0300

    Реализация Хендлера 'Login'

--- backend/internal/handler/handler.go
+++ backend/internal/handler/handler.go
@@ -41,13 +41,12 @@ func (h *AuthHandler) Register(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	defer r.Body.Close()
-
 	var req RegisterRequest
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		http.Error(w, "Invalid JSON", http.StatusBadRequest)
 		return
 	}
+	defer r.Body.Close()
 
 	if req.Email == "" || len(req.Password) < 6 {
 		http.Error(w, "Invalid email or password", http.StatusBadRequest)
@@ -65,5 +64,35 @@ func (h *AuthHandler) Register(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(map[string]string{"token": token})
+}
+
+func (h *AuthHandler) Login(w http.ResponseWriter, r http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var req LoginRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+
+	if req.Email == "" || req.Password == "" {
+		http.Error(w, "Invalid creadentials", http.StatusBadRequest)
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	token, err := h.authService.Login(ctx, req.Email, req.Password)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+	}
+
+	w.Header().Set("Content-Type", "applications/json")
 	json.NewEncoder(w).Encode(map[string]string{"token": token})
 }
